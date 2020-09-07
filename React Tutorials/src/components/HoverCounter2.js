d1ece955f6b3d945be2dfcd297ebf20717708f9d
ShahrozeMirza <mirzashahroze8@gmail.com>
2020-09-07 15:24:58 +0500

    Update HoverCounter2.js

--- React Tutorials/src/components/HoverCounter2.js
+++ React Tutorials/src/components/HoverCounter2.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class HoverCounter2 extends Component {
+class HoverCounter2 extends Component {
 
   render() {
     //We destructure the props passed to the 'HoverCounter2' component as props in the App.js file. The 'HoverCounter2' component makes use of the passed in props to render the actual UI. When you hover over the h1 and call the increment call method or try to display the count value it is pretty much what the CounterRenderProps has provided.
