860e8149eaa2404ea783f54bf762b2abdc7a3043
ShahrozeMirza <35922316+ShahrozeMirza@users.noreply.github.com>
2020-09-21 01:53:03 +0500

    useMemo hook learned

--- Hooks/remaining-hooks/src/App.js
+++ Hooks/remaining-hooks/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './components/ParentComponent';
+import UseMemoCounter from './components/UseMemoCounter';
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent/>
+      {/* <ParentComponent/> */}
+      <UseMemoCounter/>
     </div>
   );
 }
